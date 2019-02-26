65227873972dd1cd9e9bb5a5e2d22186f9d13dd2
phpjinjin <17706343618@163.com>
2019-02-26 16:51:29 +0800

    框架加后台模板

---
+++ mae/resources/views/admin/public/ifram.blade.php
@@ -0,0 +1,11 @@
+<link rel="Bookmark" href="favicon.ico" >
+<link rel="Shortcut Icon" href="favicon.ico" />
+<!--[if lt IE 9]>
+<script type="text/javascript" src="/d/lib/html5shiv.js"></script>
+<script type="text/javascript" src="/d/lib/respond.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="/d/static/h-ui/css/H-ui.min.css" />
+<link rel="stylesheet" type="text/css" href="/d/static/h-ui.admin/css/H-ui.admin.css" />
+<link rel="stylesheet" type="text/css" href="/d/lib/Hui-iconfont/1.0.8/iconfont.css" />
+<link rel="stylesheet" type="text/css" href="/d/static/h-ui.admin/skin/default/skin.css" id="skin" />
+<link rel="stylesheet" type="text/css" href="/d/static/h-ui.admin/css/style.css" />
