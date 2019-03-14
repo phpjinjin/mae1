33be60b2a10b76260cce75dec491eab7a096c69f
phpjinjin <17706343618@163.com>
2019-03-14 09:34:48 +0800

    合并3

--- mae/routes/web.php
+++ mae/routes/web.php
@@ -106,43 +106,45 @@ Route::get('welcome.blade.php',function(){
 
 
 
-
-//后台 广告表删除路由
-Route::get('/admin/adver/adver/{id}','Admin\AdverController@delete');
-//后台 广告表
-Route::resource('/admin/adver','Admin\AdverController');
-//后台文章管理上架下架
-Route::get('/admin/works/putaway/{id}','Admin\WorksController@putaway');
-//后台文章管理删除
-Route::get('/admin/works/works/{id}','Admin\WorksController@delete');
-//后台文章管理
-Route::resource('/admin/works','Admin\WorksController');
-//后台浏览订单发货
-Route::get('/admin/orders/orderr/{id}','Admin\OrdersController@orderr');
-//后台浏览订单删除
-Route::get('/admin/orders/orders/{id}','Admin\OrdersController@delete');
-//后台浏览订单发货
-Route::resource('/admin/orders','Admin\OrdersController');
-//个人中心首页
-Route::get('home/center','Home\CenterController@index');
-//个人中心订单页面
-Route::get('home/center/order','Home\CenterController@order');
-// 个人中心订单详情
-Route::get('/home/center/show/{id}','Home\CenterController@show');
-//取消订单
-Route::get('/home/center/delete/{id}','Home\CenterController@delete');
-// 文章路由
-Route::get('/home/article','Home\ArticleController@index');
-Route::get('/home/article/show/{id}','Home\ArticleController@show');
-//前台订单
-Route::get('/home/orders','Home\OrdersController@index');
-// 修改收货地址
-Route::get('/home/orders/shows/{id}','Home\OrdersController@show');
-// 提交修改地址后数据
-Route::post('/home/orders/up/{id}','Home\OrdersController@up');
-// 提交订单
-Route::post('/home/orders/create','Home\OrdersController@create');
-
+Route::group(['middleware'=>'alogin'],function(){
+	//后台 广告表删除路由
+	Route::get('/admin/adver/adver/{id}','Admin\AdverController@delete');
+	//后台 广告表
+	Route::resource('/admin/adver','Admin\AdverController');
+	//后台文章管理上架下架
+	Route::get('/admin/works/putaway/{id}','Admin\WorksController@putaway');
+	//后台文章管理删除
+	Route::get('/admin/works/works/{id}','Admin\WorksController@delete');
+	//后台文章管理
+	Route::resource('/admin/works','Admin\WorksController');
+	//后台浏览订单发货
+	Route::get('/admin/orders/orderr/{id}','Admin\OrdersController@orderr');
+	//后台浏览订单删除
+	Route::get('/admin/orders/orders/{id}','Admin\OrdersController@delete');
+	//后台浏览订单发货
+	Route::resource('/admin/orders','Admin\OrdersController');
+});
+Route::group(['middleware'=>'hlogin'],function(){
+	//个人中心首页
+	Route::get('home/center','Home\CenterController@index');
+	//个人中心订单页面
+	Route::get('home/center/order','Home\CenterController@order');
+	// 个人中心订单详情
+	Route::get('/home/center/show/{id}','Home\CenterController@show');
+	//取消订单
+	Route::get('/home/center/delete/{id}','Home\CenterController@delete');
+	// 文章路由
+	Route::get('/home/article','Home\ArticleController@index');
+	Route::get('/home/article/show/{id}','Home\ArticleController@show');
+	//前台订单
+	Route::get('/home/orders','Home\OrdersController@index');
+	// 修改收货地址
+	Route::get('/home/orders/shows/{id}','Home\OrdersController@show');
+	// 提交修改地址后数据
+	Route::post('/home/orders/up/{id}','Home\OrdersController@up');
+	// 提交订单
+	Route::post('/home/orders/create','Home\OrdersController@create');
+});
 
 
 
